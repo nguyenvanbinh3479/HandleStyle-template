1dda5692f75f558c6fad858f03141303750806bf
Bao Vu Hoang <baovuhbv@gmail.com>
2018-11-03 10:26:26 +0700

    Edited Index Page

--- src/app/index.js
+++ src/app/index.js
@@ -4,6 +4,8 @@
 
 // Load application styles
 import 'styles/_bootstrap/bootstrap.scss';
+import 'styles/_themes/_components/_header.scss'
+import 'styles/_themes/_components/_jumbotron.scss'
 import 'styles/_themes/index.scss'
 
 // ================================
