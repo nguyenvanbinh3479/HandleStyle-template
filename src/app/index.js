7678df3374521793f62ce4bfb8375f28b2adeaef
Bao Vu Hoang <baovuhbv@gmail.com>
2018-11-03 11:05:01 +0700

    Added Nav Menu

--- src/app/index.js
+++ src/app/index.js
@@ -6,6 +6,7 @@
 import 'styles/_bootstrap/bootstrap.scss';
 import 'styles/_themes/_components/_header.scss'
 import 'styles/_themes/_components/_jumbotron.scss'
+import 'styles/_themes/_components/_nav_menu.scss'
 import 'styles/_themes/index.scss'
 
 // ================================
