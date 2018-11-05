c98a536e097e2a192651ad48c962152180d724f8
Bao Vu Hoang <baovuhbv@gmail.com>
2018-11-05 22:06:39 +0700

    Fixed Responsive Navbar

--- src/app/index.js
+++ src/app/index.js
@@ -8,3 +8,16 @@ import 'styles/index.scss'
 // ================================
 // START YOUR APP HERE
 // ================================
+
+// $('[data-toggle="collapse"]').on('click', function() {
+//   $navMenuCont = $($(this).data('target'));
+//   $navMenuCont.animate({
+//     'width': 'toggle'
+//   }, 350);
+//   $(".menu-overlay").fadeIn(500);
+
+// });
+// $(".menu-overlay").click(function(event) {
+//   $(".navbar-toggle").trigger("click");
+//   $(".menu-overlay").fadeOut(500);
+// });
