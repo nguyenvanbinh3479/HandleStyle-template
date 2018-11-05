a09eec75402b98dc593dafbd20f1c7873ed34db6
Bao Vu Hoang <baovuhbv@gmail.com>
2018-11-05 22:08:40 +0700

    Remove index.js

--- src/app/index.js
+++ src/app/index.js
@@ -9,15 +9,3 @@ import 'styles/index.scss'
 // START YOUR APP HERE
 // ================================
 
-// $('[data-toggle="collapse"]').on('click', function() {
-//   $navMenuCont = $($(this).data('target'));
-//   $navMenuCont.animate({
-//     'width': 'toggle'
-//   }, 350);
-//   $(".menu-overlay").fadeIn(500);
-
-// });
-// $(".menu-overlay").click(function(event) {
-//   $(".navbar-toggle").trigger("click");
-//   $(".menu-overlay").fadeOut(500);
-// });
