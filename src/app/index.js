d108da7fe03a5b48b3f39ab848499edbe36b4aa8
binhnguyen <nguyenvanbinh3479@gmail.com>
2018-11-06 07:37:25 +0700

    modify template

--- src/app/index.js
+++ src/app/index.js
@@ -3,16 +3,14 @@
  */
 
 // Load application styles
-<<<<<<< HEAD
 import 'styles/index.scss'
-=======
 import 'styles/_bootstrap/bootstrap.scss';
 import 'styles/_themes/_components/_header.scss'
 import 'styles/_themes/_components/_jumbotron.scss'
 import 'styles/_themes/_components/_nav_menu.scss'
+import 'styles/_themes/_components/_main.scss'
 import 'styles/_themes/_components/_footer.scss'
 import 'styles/_themes/index.scss'
->>>>>>> modify template
 
 // ================================
 // START YOUR APP HERE
